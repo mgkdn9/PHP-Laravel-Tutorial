439cbf91a6ca7346bf26122cd027f4a48f9c5ce2
Mike Kohlberg <mkohlberg95@gmail.com>
2023-04-11 15:39:53 -0500

    user FK on Listing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
+
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -21,7 +27,10 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         //Using Factory
-        Listing::factory(6)->create();
+        // Listing::factory(6)->create();
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
 
         //Manually adding data
         // Listing::create([
